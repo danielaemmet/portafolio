e8b5df2d6ce95ac16faaf9a490ae9467bcc02b15
danielaemmet <dannyemet@gmail.com>
2023-08-06 19:42:15 -0600

    Se suben archivos del proyecto

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// function enviarMensaje() {
+//     var numeroTelefono = "525537060973"; 
+//     var mensaje = "Hola, ¿en qué puedo ayudarte?"; // Mensaje
+    
+//     // Codifica el mensaje para que los espacios y caracteres especiales sean válidos en la URL.
+//     mensaje = encodeURIComponent(mensaje);
+    
+//     var enlaceWhatsApp = "https://api.whatsapp.com/send?phone=" + numeroTelefono + "&text=" + mensaje;
+    
+//     // Abre una nueva ventana o pestaña para enviar el mensaje por WhatsApp.
+//     window.open(enlaceWhatsApp, "_blank");
+// }
